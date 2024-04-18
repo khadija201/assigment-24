3ad8673d92351fcad67a30279b66ee82ea469805
khadija201 <khadija2qubra@gmail.com>
2024-04-18 06:50:51 +0500

    assigment 24 done

---
+++ app.js
@@ -0,0 +1,16 @@
+function signin(){
+    console.log("submit form")
+}
+function image1(){
+    console.log("image clear")
+}
+function det(){
+    console.log("delete")
+}
+
+function over(){
+    console.log("image tragger")
+}
+function out(){
+    console.log("image tragger out")
+}
